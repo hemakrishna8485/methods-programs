76f4ace221badf0332423891fec5ca55f41b790a
hemakrishna8485 <80311987+hemakrishna8485@users.noreply.github.com>
2021-03-10 11:34:35 +0530

    Add files via upload

---
+++ sum of factors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class sum_of_factors
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(SumFactors(12));
+        }
+
+        private static int SumFactors(int v)
+        {
+            int sum = 0;
+            for (int i = 1; i <= v; i++)
+            {
+                if (v % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
+       
